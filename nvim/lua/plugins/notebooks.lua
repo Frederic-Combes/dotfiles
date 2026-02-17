68f0653d660f40c81bb5e0a499583b726f603cfc
Frédéric Combes <frederic.combes@descartesunderwriting.com>
2026-02-17 14:20:35 +0100

    wip

--- nvim/lua/plugins/notebooks.lua
+++ nvim/lua/plugins/notebooks.lua
@@ -104,11 +104,7 @@ return {
     },
     {
         "GCBallesteros/jupytext.nvim",
-        opts = {
-            style = "markdown",
-            output_extension = "md",
-            force_ft = "markdown",
-        },
+        opts = {},
         lazy = false
     }
 }
